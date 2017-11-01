69b5a18df15fdd06afc6fcb8f10aba7ad619348d
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-11-01 14:44:05 -0600

    💯  MVP and Prototype completed

--- src/components/Build.js
+++ src/components/Build.js
@@ -28,17 +28,22 @@ export class Build extends React.Component {
     return (
       <div className='sunday'>
           <div className='lowerImg'>
-          <ul>
-          <li>
-            <a>
-              <img src='https://s3-us-west-1.amazonaws.com/paper-doll/sourPuss.png' alt='top'/>
-            </a>
-            </li>
+          <ul className='list'>
             <li>
             <a>
               <img className='quadtop' src='https://s3-us-west-1.amazonaws.com/paper-doll/QuadBloxNeutral.png' alt='top'/>
             </a>
             </li>
+            <li>
+              <a>
+                <img src='https://s3-us-west-1.amazonaws.com/paper-doll/sourPuss.png' alt='top'/>
+              </a>
+              </li>
+              <li>
+                <a>
+                  <img src='https://s3-us-west-1.amazonaws.com/paper-doll/TUKwoodiePlatforms.png' alt='top'/>
+                </a>
+              </li>
             </ul>
         </div>
       </div>
